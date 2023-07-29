b1f6f2d8db03a8ccb4f888e288e2efddca6186e6
vishalpande <pandevishal40@gmail.com>
2023-07-29 14:46:41 +0530

    updating the pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import DetailsFilling from "./Pages/DetailsFilling";
+import CheckSelectedId from "./Components/CheckSelectedId";
+import AboutUs from "./Pages/AboutUs";
+import MyResumes from "./Pages/MyResumes";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Home Page Route */}
+        <Route exact path="/" element={<Home />} />
+
+        {/* Details Filling Page */}
+        <Route
+          exact
+          path="/template/fill-details"
+          element={
+            <CheckSelectedId>
+              <DetailsFilling />
+            </CheckSelectedId>
+          }
+        />
+
+        {/* My Resumes Page */}
+        <Route exact path="/my/resumes" element={<MyResumes />} />
+
+        {/* About Us Page */}
+        <Route exact path="/about-us" element={<AboutUs />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
